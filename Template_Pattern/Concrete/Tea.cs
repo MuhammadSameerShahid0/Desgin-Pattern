8b5d6f14c1b5366cebb22320ab1562e0da63ee95
MuhammadSameerShahid0 <muhammadsameershahid0@gmail.com>
2024-07-31 20:24:22 +0500

    Template_Pattern

---
+++ Template_Pattern/Concrete/Tea.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Template_Pattern.Template;
+
+namespace Template_Pattern.Concrete
+{
+    public  class Tea : PreparingTemplate
+    {
+        string Temp = "Hot";
+        public override void BoilWater()
+        {
+            Console.WriteLine($"Boiling Water {Temp}");
+        }
+        protected override void Brew()
+        {
+            Console.WriteLine("Steeping the tea");
+        }
+        protected override void AddCondiments()
+        {
+            Console.WriteLine("Adding lemon");
+        }
+    }
+}
---
+++ Template_Pattern/Template/PreparingTemplate.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Template_Pattern.Template
+{
+    public abstract class PreparingTemplate
+    {
+        // Template
+        public void PrepaingRecipe()
+        {
+            BoilWater();
+            Brew();
+            PourInCup();
+            AddCondiments();
+        }        
+        private void PourInCup()
+        {
+            Console.WriteLine("Pour In Cup");
+        }
+        public abstract void BoilWater();
+        protected abstract void Brew();
+        protected abstract void AddCondiments();
+    }
+}
