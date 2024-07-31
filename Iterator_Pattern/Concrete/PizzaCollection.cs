0d528492f749b909c68731bf00cbcae8a138dbc6
MuhammadSameerShahid0 <muhammadsameershahid0@gmail.com>
2024-07-31 20:18:38 +0500

    Iterator_Pattern

---
+++ Iterator_Pattern/Concrete/PizzaCollection.cs
@@ -0,0 +1,30 @@
+﻿using Iterator_Pattern.Interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Iterator_Pattern.Concrete
+{
+    public class PizzaCollection : IAggregate<Pizza>
+    {
+        private List<Pizza> _pizzas = new List<Pizza>();
+        public IIterator<Pizza> CreateInterface()
+        {
+            return new PizzaIterator(this);
+        }
+        public int count()
+        {
+            return _pizzas.Count();
+        }
+        public void AddPizza(Pizza pizza) 
+        { 
+            _pizzas.Add(pizza); 
+        }
+        public Pizza this[int index]
+        {
+            get { return _pizzas[index]; }
+        }
+    }
+}
---
+++ Iterator_Pattern/Concrete/PizzaIterator.cs
@@ -0,0 +1,28 @@
+﻿using Iterator_Pattern.Interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Iterator_Pattern.Concrete
+{
+    public class PizzaIterator : IIterator<Pizza>
+    {
+        private PizzaCollection _PizzaCollection;
+        private int _CurrentIndex = 0;
+        public PizzaIterator(PizzaCollection collection)
+        {
+            _PizzaCollection = collection;
+        }
+        public bool HasNext()
+        {
+            return _CurrentIndex < _PizzaCollection.count();
+        }
+
+        public Pizza Next()
+        {
+            return _PizzaCollection[_CurrentIndex++];
+        }
+    }
+}
